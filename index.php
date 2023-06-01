68a36b4275c859cdd9eb879b7d43f8c95c7dfd36
dibyowidodo <wdd.dibyo@gmail.com>
2023-06-01 16:59:50 +0000

    Added chart bg

--- index.php
+++ index.php
@@ -71,6 +71,12 @@ if($_SESSION['user_autentification'] !="valid") {
         white-space: nowrap;
         -webkit-overflow-scrolling: touch;
       }
+
+      .bg-chart {
+        background-image: url('assets/brantas_bg.png');
+        background-size: cover;
+        background-position: center;
+       }
     </style>
 
     
@@ -80,7 +86,7 @@ if($_SESSION['user_autentification'] !="valid") {
   <body>
     
 <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-  <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6" href="index.html">Basarnas Admin</a>
+  <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6" href="index.html">Disaster Management</a>
   <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -95,7 +101,7 @@ if($_SESSION['user_autentification'] !="valid") {
 <div class="container-fluid">
   <div class="row">
     <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
-      <div class="position-sticky pt-3 sidebar-sticky">
+      <div class="position-sticky pt-3 sidebar-sticky" >
         <ul class="nav flex-column">
           <li class="nav-item">
             <a class="nav-link active" aria-current="page" href="#">
@@ -135,7 +141,7 @@ if($_SESSION['user_autentification'] !="valid") {
     </nav>
 
     <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-      <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+      <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom" >
         <h1 class="h2">Dashboard</h1>
         <div class="btn-toolbar mb-2 mb-md-0">
           <div class="btn-group me-2">
@@ -164,7 +170,7 @@ if($_SESSION['user_autentification'] !="valid") {
 
       
 
-      <div class="row row-cols-1 row-cols-md-2 gx-4 text-center">
+      <div class="row row-cols-1 row-cols-md-2 gx-4 text-center bg-chart">
         <div class="col themed-grid-col chart-font">Kecepatan Arus Sungai (m/s)</div>
         <div class="col themed-grid-col chart-font">Suhu Air Sungai (&deg;C)</div>
         <div class="col themed-grid-col">
